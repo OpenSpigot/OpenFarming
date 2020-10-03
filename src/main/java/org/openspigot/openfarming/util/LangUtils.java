375080dcdc2568846a110f84d769aaba1750e199
Legend <evaninho94@gmail.com>
2020-10-03 01:50:10 +0100

    Poorly implemented farm levels

--- src/main/java/org/openspigot/openfarming/util/LangUtils.java
+++ src/main/java/org/openspigot/openfarming/util/LangUtils.java
@@ -4,6 +4,7 @@ import org.bukkit.ChatColor;
 import org.openspigot.openfarming.OpenFarming;
 import org.openspigot.openfarming.farm.FarmBlock;
 import org.openspigot.openfarming.farm.FarmType;
+import org.openspigot.openfarming.farm.upgrades.FarmUpgrades;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,12 +30,19 @@ public class LangUtils {
     }
 
     public static String parseFarmMessage(String message, FarmBlock farm) {
-
         return ChatColor.translateAlternateColorCodes('&', message
-                .replaceAll("\\{FARM-RADIUS}" , String.valueOf(farm.getRadius()))
-                .replaceAll("\\{FARM-SPEED}"  , String.valueOf(farm.getSpeed()))
+                .replaceAll("\\{FARM-RADIUS}" , String.valueOf(FarmUpgrades.RADIUS_UPGRADE.getLevel(farm.getRadius() + 1).getValue()))
+                .replaceAll("\\{FARM-SPEED}"  , String.valueOf(FarmUpgrades.SPEED_UPGRADE.getLevel(farm.getSpeed() + 1).getValue()))
                 .replaceAll("\\{FARM-TYPE}"   , translateFarmType(farm.getType()))
                 .replaceAll("\\{FARM-REPLANT}", toYesNo(farm.isReplant()))
+
+                .replaceAll("\\{NEXT-RADIUS}", String.valueOf(FarmUpgrades.RADIUS_UPGRADE.getLevel(farm.getRadius() + 1).getValue()))
+                .replaceAll("\\{NEXT-RADIUS-BALCOST}", String.valueOf(FarmUpgrades.RADIUS_UPGRADE.getLevel(farm.getRadius() + 1).getEcoCost()))
+                .replaceAll("\\{NEXT-RADIUS-EXPCOST}", String.valueOf(FarmUpgrades.RADIUS_UPGRADE.getLevel(farm.getRadius() + 1).getExpCost()))
+
+                .replaceAll("\\{NEXT-SPEED}", String.valueOf(FarmUpgrades.SPEED_UPGRADE.getLevel(farm.getSpeed() + 1).getValue()))
+                .replaceAll("\\{NEXT-SPEED-BALCOST}", String.valueOf(FarmUpgrades.SPEED_UPGRADE.getLevel(farm.getSpeed() + 1).getEcoCost()))
+                .replaceAll("\\{NEXT-SPEED-EXPCOST}", String.valueOf(FarmUpgrades.SPEED_UPGRADE.getLevel(farm.getSpeed() + 1).getExpCost()))
         );
     }
 }
